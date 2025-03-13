2d65dbdf16995dfe3600cea8e3a6d67afec595f1
Manohar Ravula <99091645+manoharyadavr@users.noreply.github.com>
2025-03-13 13:33:06 +0530

    Update Appointments.js

--- src/components/Appointments.js
+++ src/components/Appointments.js
@@ -12,7 +12,7 @@ const Appointments = () => {
     reason: "",
   });
 
-  const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000"; 
+  const API_BASE_URL = "https://hospital-website-v1-1.onrender.com" || "http://localhost:5000"; 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
